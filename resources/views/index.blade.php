780f7993cfdec82a67e8719862302b50f714c8e3
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-20 00:45:32 +0530

    google tag added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7B7LE2W26N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7B7LE2W26N');
+</script>
 <!-- Basic -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
