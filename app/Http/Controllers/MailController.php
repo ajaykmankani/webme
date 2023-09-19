e5d8eb950c7a41a19476ec043ff10d5b26c4e725
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-19 19:43:22 +0530

    mail system check

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\QueryMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    public function index(){
+        $mailData = [
+            'title'=>'Testing Mail',
+            'body'=>'This is a test mail.'
+        ];
+
+        Mail::to('inquery@webmestudio.com')->send(new QueryMail($mailData));
+        dd('mail sent successfully');
+    }
+}
