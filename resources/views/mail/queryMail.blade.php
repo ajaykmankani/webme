f989f26040416459682262688f939c987495e67f
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-19 20:07:52 +0530

    mail system correction 5

--- resources/views/mail/queryMail.blade.php
+++ resources/views/mail/queryMail.blade.php
@@ -1,8 +1,19 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p class="align-center">{{ $mailData['title'] }}</p>
 
 
-{{ $mailData->title }}
+<p>{{ $mailData['body'] }}</p>
+<p > Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, beatae unde. Eum, omnis, iste provident tempore commodi sapiente, vero dolorum soluta cum rem quod ad. Totam ullam nisi omnis quisquam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit saepe excepturi, impedit odio quos, omnis fuga doloribus aliquid voluptates autem aspernatur ipsam eum porro explicabo obcaecati iste molestiae tempore blanditiis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur rem maxime sapiente, voluptatibus obcaecati alias asperiores laboriosam odio eligendi voluptatum error impedit consectetur reprehenderit unde qui quam fuga suscipit distinctio.</p>
+</body>
+</html>
 
-<br>
-<br>
-{{ $mailData->body }}
+
