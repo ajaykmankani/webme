b1a6c858d0223535ed38da07ccc2a6a354f3e2c2
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-19 20:56:52 +0530

    test mail correction

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\QueryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 
 
 Route::get('send_mail', [MailController::class, 'index']);
+Route::resource(QueryController::class);
