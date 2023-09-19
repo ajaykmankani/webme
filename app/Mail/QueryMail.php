6fe895ad192b72140b6469a7efc6dc9233f38f0d
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-19 20:01:56 +0530

    mail system correction 3

--- app/Mail/QueryMail.php
+++ app/Mail/QueryMail.php
@@ -38,7 +38,7 @@ class QueryMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'views.mail.queryMail',
+            view: 'mail.queryMail',
         );
     }
 
