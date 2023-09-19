67818bd32387ae753267875e356fad7f1c96ebf2
Ajay Mankani <ajaykmankani66@gmail.com>
2023-09-19 20:55:05 +0530

    test mail

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -570,23 +570,24 @@
 				<div class="col-md-12">
 					<div class="contact_form">
 						<div id="message"></div>
-						<form id="contactForm" name="sentMessage" novalidate="novalidate">
+						<form id="contactForm" name="sentMessage" action="{{ route('send_query') }}" method="POST">
+                            @csrf
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group">
-										<input class="form-control" id="name" type="text" placeholder="Your Name"
+										<input class="form-control" name="name" id="name" type="text" placeholder="Your Name"
 											required="required"
 											data-validation-required-message="Please enter your name.">
 										<p class="help-block text-danger"></p>
 									</div>
 									<div class="form-group">
-										<input class="form-control" id="email" type="email" placeholder="Your Email"
+										<input class="form-control" name="email" id="email" type="email" placeholder="Your Email"
 											required="required"
 											data-validation-required-message="Please enter your email address.">
 										<p class="help-block text-danger"></p>
 									</div>
 									<div class="form-group">
-										<input class="form-control" id="phone" type="tel" placeholder="Your Phone"
+										<input class="form-control" name="phone" id="phone" type="tel" placeholder="Your Phone"
 											required="required"
 											data-validation-required-message="Please enter your phone number.">
 										<p class="help-block text-danger"></p>
@@ -594,7 +595,7 @@
 								</div>
 								<div class="col-md-6">
 									<div class="form-group">
-										<textarea class="form-control" id="message" placeholder="Your Message"
+										<textarea class="form-control" name="message" id="message" placeholder="Your Message"
 											required="required"
 											data-validation-required-message="Please enter a message."></textarea>
 										<p class="help-block text-danger"></p>
